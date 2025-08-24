31fac10a14b787db3af0e6a3a6d0de8c5a3f7320
unknown <adimak212@gmail.com>
2025-08-24 20:26:48 +0300

    ok

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-import express from "express"
-import cors from "cors"
-import bodyParser from "body-parser"
-import { router } from "./routers/index.js"
-
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { router } from "./routers/index.js";
 
 const corsOptions = {
-    origin:[
-        "http://localhost:5173"
-    ]
-}
+  origin: ["http://localhost:5173"],
+};
 
+const app = express();
 
-const app  = express()
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(router);
 
-app.use(cors(corsOptions))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(router)
-
-app.listen(3000, () => {
-})
+app.listen(3000, () => {});
--- routers/index.js
+++ routers/index.js
@@ -1,11 +1,12 @@
 import express from "express";
-import {post} from "../controllers/post.js"
+import { post } from "../controllers/post.js";
 import { getAll } from "../controllers/getAll.js";
 import { deleteById } from "../controllers/deleteById.js";
-
+import multer from "multer";
+const upload = multer({ storage: multer.memoryStorage() });
 
 export const router = express.Router();
 
-router.post("/post" , post);
-router.get("/getAll" , getAll);
-router.delete("/deleteById" , deleteById);
+router.post("/post", upload.single("file"), post);
+router.get("/getAll", getAll);
+router.delete("/deleteById", deleteById);
